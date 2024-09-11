5d052d34045a7cebe1e42476c8181a0fa7c0d0f1
pedro01p <tsukki237@gmail.com>
2024-09-11 15:49:58 -0300

    first commit

---
+++ atividade013.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+val=float(input("informe o numero"))
+if val<0:
+    print("seu numeo é negativo")
+if val==0:
+    print("seu numero é zero")
+if val>0:
+    print("seu numero é positivo")
